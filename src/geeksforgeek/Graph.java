eab9d6c6d40ec548e52f41fa611edaad429d2a88
Jeonggyu Lee <glaze@whyi.net>
2015-01-02 23:19:44 -0800

    committing ongoing changes

--- src/geeksforgeek/Graph.java
+++ src/geeksforgeek/Graph.java
@@ -3,13 +3,14 @@ package geeksforgeek;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
 import org.junit.Test;
-
+import static org.junit.Assert.*;
 class Vertex {
 	public ArrayList<Vertex> neighbors = null;
 	public Integer data;
@@ -36,48 +37,73 @@ public class Graph {
 		vertices = new ArrayList<Vertex>();
 	}
 
-	private Vertex getvertexAt(int index) {
+	private Vertex getVertexAt(int index) {
 		try {
 			return vertices.get(index);
 		}
 		catch (IndexOutOfBoundsException e) {
-			System.out.println("addEdge : no such vertex " + index);
+			System.out.println("getVertexAt : no such vertex " + index);
 			return null;
 		}		
 	}
 	
-	public boolean addEdge(int from, int to) {
-		final Vertex fromvertex = getvertexAt(from);
-		final Vertex tovertex = getvertexAt(to);
+	public boolean addEdgeMutually(int from, int to) {
+		final Vertex fromVertex = getVertexAt(from);
+		final Vertex toVertex = getVertexAt(to);
 		
-		if (fromvertex != null && tovertex != null) {
-			fromvertex.addNeighbor(tovertex);
-			tovertex.addNeighbor(fromvertex);
+		if (fromVertex != null && toVertex != null) {
+			fromVertex.addNeighbor(toVertex);
+			toVertex.addNeighbor(fromVertex);
 			return true;
 		}
 		
 		return false;
 	}
 	
-	public void addvertex(Vertex vertex) {
+	public boolean addEdge(int from, int to) {
+		final Vertex fromVertex = getVertexAt(from);
+		final Vertex toVertex = getVertexAt(to);
+		
+		if (fromVertex != null && toVertex != null) {
+			fromVertex.addNeighbor(toVertex);
+			return true;
+		}
+		
+		return false;
+	}	
+	
+	public void addVertex(Vertex vertex) {
 		vertices.add(vertex);
 	}
 	
 	//http://www.geeksforgeeks.org/graph-and-its-representations/
 	private void constructGraph() {
+		vertices.clear();
 		// 5 vertices, 0 1 2 3 4
 		for (int i = 0; i < 5; ++i) {
-			addvertex(new Vertex(i));
+			addVertex(new Vertex(i));
+		}
+		
+		addEdgeMutually(0,1);
+		addEdgeMutually(0,4);
+		addEdgeMutually(1,4);
+		addEdgeMutually(1,3);
+		addEdgeMutually(4,3);
+		addEdgeMutually(1,2);
+		addEdgeMutually(3,2);
+	}
+	
+	private void constructGraphWithoutCycle() {
+		vertices.clear();
+		// 5 vertices, 0 1 2 3
+		for (int i = 0; i < 4; ++i) {
+			addVertex(new Vertex(i));
 		}
 		
 		addEdge(0,1);
-		addEdge(0,4);
-		addEdge(1,4);
-		addEdge(1,3);
-		addEdge(4,3);
-		addEdge(1,2);
-		addEdge(3,2);
-	}
+		addEdge(2,0);
+		addEdge(2,3);
+	}	
 	
 	public void printGraph() {
 		System.out.println("-------------------Dumping Graph----------------");
@@ -171,16 +197,48 @@ public class Graph {
 		System.out.println("\n-----------------------Done---------------------");
 	}
 	
+	public boolean hasCycle() {
+		Stack<Vertex> stack = new Stack<Vertex>();
+		HashMap<Vertex, Boolean> visited = new HashMap<Vertex, Boolean>();
+		
+		stack.push(vertices.get(0));
+
+		while (!stack.isEmpty()) {
+			Vertex vertex = stack.pop();
+			if (!visited.containsKey(vertex)) {
+				visited.put(vertex, true);
+				for (Vertex v : vertex.neighbors) {
+					stack.push(v);
+				}
+				continue;
+			}
+			if (visited.containsKey(vertex)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
 	@Test
 	public void test() {
 		constructGraph();
 		printGraph();
 		
 		// O(V+E)
-		for (int i = 0; i < 10; ++i) {
-			printBreadthFirstTraversal(i);
-			printDepthFirstTraversal(i);
-		}
+		ArrayList<Integer> expectedBFS = new ArrayList<Integer>(Arrays.asList(new Integer[]{0,1,4,3,2}));
+		assertEquals(expectedBFS, breadthFirstTraversal(0));
+		ArrayList<Integer> expectedDFS = new ArrayList<Integer>(Arrays.asList(new Integer[]{0,4,3,2,1}));
+		assertEquals(expectedDFS, depthFirstTraversal(0));
 	}
+	
+	@Test
+	public void detectCycle() {
+		// O(V+E)
+		constructGraph();
+		assertEquals(true, hasCycle());
 
+		constructGraphWithoutCycle();
+		assertEquals(false, hasCycle());
+	}
 }
