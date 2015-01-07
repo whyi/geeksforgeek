fc3b379f350a3a2c803d1dc6f21d606192f315d3
Jeonggyu Lee <glaze@whyi.net>
2015-01-06 17:28:15 -0800

    committing the ongoing changes

--- src/geeksforgeek/LinkedIn.java
+++ src/geeksforgeek/LinkedIn.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Stack;
 
 import org.junit.Test;
 
@@ -767,5 +768,114 @@ TreeNode LowestCommonAncestorLoop(TreeNode currentNode, int Node1Value, int Node
 		ArrayList<ArrayList<Integer>> returned = generate(a);
 		System.out.println("total result:");
 		System.out.println(returned.toString());
+	}
+	
+	/* In "the 100 game," two players take turns adding, to a running 
+	total, any integer from 1..10. The player who first causes the running 
+	total to reach or exceed 100 wins.
+	What if we change the game so that players cannot re-use integers? 
+	For example, if two players might take turns drawing from a common pool of numbers
+	of 1..15 without replacement until they reach a total >= 100. This problem is 
+	to write a program that determines which player would win with ideal play.
+
+	Write a procedure, "Boolean canIWin(int maxChoosableInteger, int desiredTotal)",
+	which returns true if the first player to move can force a win with optimal play.
+
+	Your priority should be programmer efficiency; don't focus on minimizing
+	either space or time complexity.
+	*/
+
+	private Boolean yourPlay() {
+		return true;
+	}
+	
+	private Boolean canIWin(boolean[] choices, int remainingTotal) {
+		if (remainingTotal <= 0) {
+			return false;
+		}
+
+		boolean result = false;
+
+		for (int i = 0; i < choices.length; ++i) {
+			if (choices[i]) {
+				choices[i] = false;
+				
+				int currentTotal = remainingTotal-i-1;
+				if (currentTotal == 0) {
+					return true;
+				}
+				
+				// the other player
+				boolean cur = true;
+				for (int j = 0; j < choices.length; ++j) {
+					if (choices[i]) {
+						choices[i] = false;
+						cur = canIWin(choices, remainingTotal - j - i);
+						choices[i] = true;
+					}
+				}
+				result = result || cur;
+				choices[i] = true;
+			}
+		}
+		return result;
+	}
+	
+	public Boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+		// 	Implementation here. Write yours
+		
+		boolean[] choices = new boolean[maxChoosableInteger];
+		for (int i = 0; i < maxChoosableInteger; ++i) {
+			choices[i] = true;
+		}
+		boolean myTurn = true;
+		return canIWin(choices, desiredTotal);
+	}
+	
+	@Test
+	public void canIWin() {
+		System.out.println("can I win? " + canIWin(6, 12));
+		System.out.println("false||true : " + (false||true));
+	}
+	
+	@Test
+	// http://www.careercup.com/question?id=6266917077647360
+	public void convertBinaryTree() {
+		BSTNode root = new BSTNode(1);
+		root.left = new BSTNode(2);
+		root.right = new BSTNode(3);
+		root.left.left = new BSTNode(4);
+		root.left.right = new BSTNode(5);
+		root.left.left.left = new BSTNode(6);
+		root.left.left.right = new BSTNode(7);
+		
+		BSTNode node = root;
+		Stack<BSTNode> stack = new Stack<BSTNode>();
+		while (node != null) {
+			stack.push(node);
+			node = node.left;
+		}
+		
+		BSTNode prev = null;
+		while (!stack.isEmpty()) {
+			node = stack.pop();
+			// this is the root
+			if (node.left == null && node.right == null) {
+				System.out.println("top : " + node.data);
+				root = node;
+			}
+			
+			if (node.right != null) {
+				prev.left = node.right;
+				prev.right = node;
+				node.right = null;
+			}
+			prev = node;
+		}
+		node.left = null;
+		node.right = null;
+		System.out.println("flipped!!!!!! " + root.data);
+		Helper.levelOrder(root);
+		System.out.println("\nflipped!!!!!! done" + root.data);
 	}
 }
